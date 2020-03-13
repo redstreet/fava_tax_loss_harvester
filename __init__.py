428af332700d114de8b7f52cb68c22336e0d72f0
Red S <redstreet@users.noreply.github.com>
2020-03-13 00:49:54 -0700

    add "by commodity" view; move summary to top

--- __init__.py
+++ __init__.py
@@ -77,19 +77,43 @@ class TaxLossHarvester(FavaExtensionBase):  # pragma: no cover
                 to_sell.append(RetRow(row.account, row.units, row.acquisition_date, 
                     row.market_value, loss, wash))
 
+        harvestable_table = retrow_types, to_sell
+        harvestable_by_commodity = self.harvestable_by_commodity(retrow_types, to_sell)
+
         # Summary
         locale.setlocale(locale.LC_ALL, '')
         summary = {}
+        summary["Total harvestable loss"] = sum(i.loss for i in to_sell)
+        summary["Total sale value required"] = sum(i.market_value.get_only_position().units.number for i in to_sell)
+        summary["Commmodities with a loss"] = len(harvestable_by_commodity[1])
         summary["Total transactions"] = len(to_sell)
         unique_txns = set((r.account, r.units.get_only_position().units.currency) for r in to_sell)
         summary["Total unique transactions"] = len(unique_txns)
-        summary["Total harvestable loss"] = sum(i.loss for i in to_sell)
-        summary["Total sale value required"] = sum(i.market_value.get_only_position().units.number for i in to_sell)
         summary = {k:'{:n}'.format(int(v)) for k,v in summary.items()}
 
-        harvestable_table = retrow_types, to_sell
+
         recents = self.build_recents(recently_bought)
-        return harvestable_table, summary, recents
+        return harvestable_table, summary, recents, harvestable_by_commodity
+
+    def harvestable_by_commodity(self, rtype, rrows):
+        """Group input by sum(commodity)
+        """
+
+        retrow_types = [('currency', str), ('total_units', Decimal), ('total_loss', Decimal)]
+        RetRow = collections.namedtuple('RetRow', [i[0] for i in retrow_types])
+
+        units = collections.defaultdict(Decimal)
+        losses = collections.defaultdict(Decimal)
+        for row in rrows:
+            ticker = row.units.get_only_position().units.currency
+            units[ticker] += row.units.get_only_position().units.number
+            losses[ticker] += row.loss
+
+        by_commodity = []
+        for t in units:
+            by_commodity.append(RetRow(t, units[t], losses[t]))
+
+        return retrow_types, by_commodity
 
     def build_recents(self, recently_bought):
         retval = []
