5988ef3e001a289472a61a11ea013481e56ca129
Red S <redstreet@users.noreply.github.com>
2020-03-15 03:00:35 -0700

    cleaned up test_libtlh by using an inner wrapper

--- test_libtlh.py
+++ test_libtlh.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-# string = x
-# format x
-# write string to file
-# instantiate query_func with that file
-
 from beancount.utils import test_utils
 from beancount import loader
 from beancount.query import query
@@ -13,55 +8,43 @@ import libtlh
 import tempfile
 import datetime
 
-def docfile_handle(function, **kwargs):
-    """A decorator that write the function's docstring to a temporary file
-    and calls the decorated function with the temporary file handle.  This is
-    useful for writing tests.
+@functools.lru_cache(maxsize=1)
+def dates():
+    def minusdays(today, d):
+        return (today - datetime.timedelta(days=d)).isoformat()
+    today = datetime.datetime.now().date()
+    retval = {'today': today,
+              'm1': minusdays(today, 1),
+              'm5': minusdays(today, 5),
+              'm10': minusdays(today, 10),
+              'm15': minusdays(today, 15),
+              'm20': minusdays(today, 20),
+              }
+    return retval
 
-    Args:
-      function: A function to decorate.
-    Returns:
-      The decorated function.
-    """
+def insert_dates(function, **kwargs):
+    """A decorator that rewrites the function's docstring with dates"""
+
     @functools.wraps(function)
-    def new_function(self):
-        allowed = ('buffering', 'encoding', 'newline', 'dir', 'prefix', 'suffix')
-        if any([key not in allowed for key in kwargs]):
-            raise ValueError("Invalid kwarg to docfile_extra")
-        with tempfile.NamedTemporaryFile('w', **kwargs) as file:
-            return function(self, file)
-    new_function.__doc__ = None
+    def new_function(self, filename):
+        return function(self, filename)
+    new_function.__doc__ = function.__doc__.format(**(dates()))
     return new_function
 
-class TestScriptCheck(test_utils.TestCase):
-    def write_file(self, f, text):
-        # write to temp file and reset it so it can be read by loader.load_file
-        f.write(text)
-        f.flush()
-        f.seek(0)
-        self.f = f
 
-    def dates(self):
-        def minusdays(today, d):
-            return (today - datetime.timedelta(days=d)).isoformat()
-        today = datetime.datetime.now().date()
-        retval = {'today': today,
-                  'm1': minusdays(today, 1),
-                  'm5': minusdays(today, 5),
-                  'm10': minusdays(today, 10),
-                  'm15': minusdays(today, 15),
-                  'm20': minusdays(today, 20),
-                  }
-        return retval
+class TestScriptCheck(test_utils.TestCase):
+    def setUp(self):
+        self.options = {'accounts_pattern': "Assets:Investments:Taxable", 'wash_pattern': "Assets:Investments"}
 
     def query_func(self, sql):
-        entries, _, options_map = loader.load_file(self.f.name)
+        entries, _, options_map = loader.load_file(self.f)
         rtypes, rrows = query.run_query(entries, options_map, sql)
         return rtypes, rrows
 
-    @docfile_handle
+    @test_utils.docfile
+    @insert_dates
     def test_no_relevant_accounts(self, f):
-        input_text = """
+        """
         2010-01-01 open Assets:Investments:Brokerage
         2010-01-01 open Assets:Bank
 
@@ -70,20 +53,19 @@ class TestScriptCheck(test_utils.TestCase):
          Assets:Bank
 
         {m1} price BNCT 100 USD
-        """.format(**(self.dates()))
-        self.write_file(f, input_text)
+        """
+        self.f = f
 
-        options = {'accounts_pattern': "Assets:Investments:Taxable", 'wash_pattern': "Assets:Investments"}
-        retrow_types, to_sell, recent_purchases = libtlh.find_harvestable_lots(self.query_func, options)
+        retrow_types, to_sell, recent_purchases = libtlh.find_harvestable_lots(self.query_func, self.options)
 
         self.assertEqual(0, len(to_sell))
         self.assertEqual(0, len(recent_purchases))
 
 
-    @docfile_handle
+    @test_utils.docfile
+    @insert_dates
     def test_harvestable_basic(self, f):
-
-        input_text = """
+        """
         2010-01-01 open Assets:Investments:Taxable:Brokerage
         2010-01-01 open Assets:Bank
 
@@ -92,11 +74,10 @@ class TestScriptCheck(test_utils.TestCase):
          Assets:Bank
 
         {m1} price BNCT 100 USD
-        """.format(**(self.dates()))
-        self.write_file(f, input_text)
+        """
+        self.f = f
 
-        options = {'accounts_pattern': "Assets:Investments:Taxable", 'wash_pattern': "Assets:Investments"}
-        retrow_types, to_sell, recent_purchases = libtlh.find_harvestable_lots(self.query_func, options)
+        retrow_types, to_sell, recent_purchases = libtlh.find_harvestable_lots(self.query_func, self.options)
 
         self.assertEqual(1, len(to_sell))
         self.assertEqual(1, len(recent_purchases))
